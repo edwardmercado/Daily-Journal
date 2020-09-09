68b0e968c261078317427732c3526b79f9b432a8
edwardmercado <edwardallenmercado@gmail.com>
2020-09-09 23:08:08 +0800

    Removed MongoDB Client

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const { MongoClient } = require("mongodb");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
