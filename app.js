40ac1fb0149cde5f5cf8a756d6d4241d5370e8f1
edwardmercado <edwardallenmercado@gmail.com>
2020-09-09 22:34:23 +0800

    Refactor to store data in MongoDB | Used Heroku config vars for secrets

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const mongoURI = "mongodb://localhost:27017/blogDB";
+// const mongoURI = "mongodb://localhost:27017/blogDB";
+
+const mongoURI = process.env.DB_URI;
 
 mongoose.connect(mongoURI, {useNewUrlParser: true});
 
@@ -44,6 +46,14 @@ const defaultContact = new Content({
 });
 
 const defaultContent = [defaultHome, defaultAbout, defaultContact];
+
+//blog collection
+const blogSchema = {
+  blogTitle: String,
+  blogBody: String,
+}
+
+const Blog = mongoose.model("Blog", blogSchema);
 
 
 app.get("/", function(req, res){
@@ -73,10 +83,17 @@ app.get("/", function(req, res){
           if (error){
             console.log(error);
           }
-          res.render("home", { homeContentTitle: foundItems.contentTitle, homeContentBody: foundItems.contentBody , submittedPost: posts });
+
+          Blog.find({}, function(blogErr, blogFoundItems){
+            if (blogErr){
+              console.log(blogErr);
+            }
+
+            res.render("home", { homeContentTitle: foundItems.contentTitle, homeContentBody: foundItems.contentBody , submittedPost: blogFoundItems });
+          });
+
         });
 
-        
     }
 
   });
@@ -115,15 +132,16 @@ app.get("/compose", function(req, res){
 
 
 app.post("/compose", function(req, res){
+
   let postTitle = req.body.postTitle;
   let postBody = req.body.postBody;
+  
+  const blog = new Blog({
+    blogTitle: postTitle,
+    blogBody: postBody
+  });
 
-  let post = {
-    title: postTitle,
-    body: postBody
-  }
-
-  posts.push(post);
+  blog.save();
 
   res.redirect("/");
 
@@ -133,23 +151,27 @@ app.post("/compose", function(req, res){
 app.get("/posts/:post", function(req, res){
   //console.log(req.params.post)
 
-  
-  posts.forEach(function (element){
+    Blog.find({}, function(blogErr, blogFoundItems){
+      if (blogErr){
+        console.log(blogErr);
+      }
 
-    if(_.lowerCase(req.params.post) === _.lowerCase(element.title)){
+      blogFoundItems.forEach(function (element){
 
-      res.render("post", {postContent: element});
+        if( _.lowerCase(req.params.post) === _.lowerCase(element.blogTitle) ){
 
-    } else {
-      console.log("Match not found");
-    }
+          res.render("post", {postContent: element});
+    
+        } else {
 
+          console.log("Match not found");
 
+        }
 
+      });
 
-  });
-  
-  
+    });  
+
 });
 
 
